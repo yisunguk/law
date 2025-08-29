fd11213ae274ed807b3845234b10a0e66d4a8c79
이성욱(Sung-Uk Yi) <149710@poscoenc.com>
2025-08-29 09:52:01 +0900

    Ddd

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,9 @@
-# modules/__init__.py  (수정본)
-
-# 패키지 내부 상대 임포트 우선
+# === modules/__init__.py (교체) ===
 try:
     from .advice_engine import AdviceEngine, pick_mode
     from .legal_modes import Intent, classify_intent, build_sys_for_mode
 except Exception:
-    # 드물게 패키지 인식이 꼬인 환경 폴백
+    # 패키지 인식이 꼬인 환경 폴백
     from advice_engine import AdviceEngine, pick_mode  # type: ignore
     from legal_modes import Intent, classify_intent, build_sys_for_mode  # type: ignore
 
